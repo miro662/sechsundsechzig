40f34f30a6aad58232a882b9615c9083f1438629
Mirosław Błażej <miro662@gmail.com>
2023-03-01 11:19:24 +0100

    Extract components

---
+++ packages/chat/components/prompt.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function Prompt({onSendMessage}: {
+    onSendMessage: (content: string) => void
+}) {
+    const [content, setContent] = useState("");
+
+    return <div>
+        <form onSubmit={ev => {
+            onSendMessage(content);
+            ev.preventDefault();
+            setContent("");
+        }}>
+            <input type="text" value={content} onChange={
+                ev => setContent(ev.target.value)
+            }/>
+            <button type="submit">Send</button>
+       </form>
+    </div>
+}
